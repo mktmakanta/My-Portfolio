32e902df5ed0b12a9bd212ff8f7802b7478e1317
mktmakanta <mktmakantamkt@gmail.com>
2024-08-14 21:14:47 +0100

    will be working with framer on this project now

--- my-project/src/components/About.jsx
+++ my-project/src/components/About.jsx
@@ -17,26 +17,26 @@ export default function About() {
             </h2>
             <div className="text-justify md:text-lg overflow-hidden">
               <p>
-                Back in 2012, I decided to try my hand at creating custom Tumblr
-                themes and tumbled head first into the rabbit hole of coding and
-                web development. Fast-forward to today, and I’ve had the
-                privilege of building software for an advertising agency, a
-                start-up, a huge corporation, and a digital product studio.
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. A
+                voluptatibus nostrum dolores porro sit id itaque eum. Error
+                architecto in esse debitis vitae obcaecati ex totam! Dolores
+                sapiente non reiciendis.
               </p>
 
               <p>
-                My main focus these days is building accessible user interfaces
-                for our customers at Klaviyo. I most enjoy building software in
-                the sweet spot where design and engineering meet — things that
-                look good but are also built well under the hood. In my free
-                time, I've also released an online video course that covers
-                everything you need to know to build a web app with the Spotify
-                API.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi,
+                sint ipsa excepturi minus natus commodi consectetur, dignissimos
+                deserunt odio maxime magnam ad blanditiis qui reprehenderit
+                quisquam? Minima earum, aperiam rerum facilis quod nobis totam
+                id. Aut repellat pariatur, quisquam nobis illum sit laborum
+                exercitationem tenetur!
               </p>
               <p>
-                When I’m not at the computer, I’m usually rock climbing,
-                reading, hanging out with my wife and two cats, or running
-                around Hyrule searching for Korok seeds K o r o k s e e d s .
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed
+                praesentium voluptas aperiam officia ipsum, et tenetur
+                reiciendis ducimus minus consectetur sit, quasi magnam. Commodi
+                eos nulla officia distinctio suscipit! Natus dolores fugit
+                molestiae reiciendis.
               </p>
             </div>
           </div>
